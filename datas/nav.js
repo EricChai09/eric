9046bc754b1924be5b3f36918f4b03e25c435144
EricChai09 <chaishengaa@163.com>
2017-12-23 16:34:46 +0800

    采购项目系统

---
+++ datas/nav.js
@@ -0,0 +1,79 @@
+var navs = [{
+	"title": "物品库",
+	"icon": "fa-cubes",
+	"spread": true,
+	"children": [{
+		"title": "医药物品",
+		"icon": "fa-cubes",
+		"href": "myyiyao.html"
+	}, {
+		"title": "装修物品",
+		"icon": "fa-cubes",
+		"href": "form.html"
+	}, {
+		"title": "办公日用品",
+		"icon": "fa-cubes",
+		"href": "table.html"
+	}, {
+		"title": "大型活动用品",
+		"icon": "fa-cubes",
+		"href": "nav.html"
+	}, {
+		"title": "其他",
+		"icon": "fa-cubes",
+		"href": "auxiliar.html"
+	}]
+}, {
+	"title": "采购",
+	"icon": "fa-cogs",
+	"spread": false,
+	"children": [{
+		"title": "日常采购",
+		"icon": "fa-table",
+		"href": "btable.html"
+	}, {
+		"title": "应急采购",
+		"icon": "fa-navicon",
+		"href": "navbar.html"
+	}, {
+		"title": "采购统计",
+		"icon": "&#xe62a;",
+		"href": "tab.html"
+	}]
+}, {
+	"title": "维护",
+	"icon": "&#x1002;",
+	"spread": false,
+	"children": [{
+        "title": "日常维护",
+        "icon": "fa-check-square-o",
+        "href": "icheck.html"
+    }, {
+        "title": "应急维护",
+        "icon": "fa-navicon",
+        "href": "navbar.html"
+    }, {
+        "title": "维护统计",
+        "icon": "&#xe62a;",
+        "href": "tab.html"
+	}]
+}, {
+	"title": "供应商",
+	"icon": "fa-address-book",
+	"href": "",
+	"spread": false,
+	"children": [{
+		"title": "医药供应商",
+		"icon": "fa-address-book",
+		"href": "https://www.github.com/"
+	}, {
+		"title": "装修供应商",
+		"icon": "fa-address-book",
+		"href": "http://www.qq.com/"
+	}, {
+		"title": "其他供应商",
+		"icon": "fa-address-book",
+		"href": "http://fly.layui.com/"
+	}]
+
+}];
